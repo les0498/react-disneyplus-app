530700a6939190967ac7bd7f310704fd2f68d059
eunseo <92281453+les0498@users.noreply.github.com>
2024-06-11 23:54:53 +0900

    Update Category.js

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import styled from 'styled-components';
 
 const Category = () => {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="disney" />
+        <video autoPlay loop muted>
+          <source src="/videos/disney.mp4" type='video/mp4' />
+        </video>
+      </Wrap>
+    </Container>
   )
 }
 
 export default Category
+
+const Container = styled.div`
+`;
+
+const Wrap = styled.div`
+`;
+
+
