fe582ece581de5c235cd50c244a6bf0d75e7b37c
eunseo <92281453+les0498@users.noreply.github.com>
2024-08-01 02:48:03 +0900

    Update useOnClickOutside.js

--- src/hooks/useOnClickOutside.js
+++ src/hooks/useOnClickOutside.js
@@ -2,6 +2,19 @@ import { useEffect } from "react";
 
 export default function useOnClickOutside(ref, handler) {
   useEffect(() => {
-    return () => {};
+    const listener = (event) => {
+      //console.log("event.target", event.target);
+      if(!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+  return () => {
+    document.removeEventListener("mousedown", listener);
+    document.removeEventListener("touchstart", listener);
+    };
   }, [ref, handler]);
 }
