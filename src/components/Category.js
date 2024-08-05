95d1d5fbb2c8a7429b526d9ae47770c7a8d7e883
eunseo <92281453+les0498@users.noreply.github.com>
2024-08-05 14:28:37 +0900

    Update Category.js

--- src/components/Category.js
+++ src/components/Category.js
@@ -5,33 +5,33 @@ const Category = () => {
   return (
     <Container>
       <Wrap>
-        <img src="/images/viewers-disney.png" alt="disney" />
+        <img src={`${process.env.PUBLIC_URL}/images/viewers-disney.png`} alt="disney" />
         <video autoPlay loop muted>
-          <source src="/videos/disney.mp4" type='video/mp4' />
+          <source src={`${process.env.PUBLIC_URL}/videos/disney.mp4`} type='video/mp4' />
         </video>
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-marvel.png" alt="disney" />
+        <img src={`${process.env.PUBLIC_URL}/images/viewers-marvel.png`} alt="disney" />
         <video autoPlay loop muted>
-          <source src="/videos/marvel.mp4" type='video/mp4' />
+          <source src={`${process.env.PUBLIC_URL}/videos/marvel.mp4`} type='video/mp4' />
         </video>
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-pixar.png" alt="disney" />
+        <img src={`${process.env.PUBLIC_URL}/images/viewers-pixar.png`} alt="disney" />
         <video autoPlay loop muted>
-          <source src="/videos/pixar.mp4" type='video/mp4' />
+          <source src={`${process.env.PUBLIC_URL}/videos/pixar.mp4`} type='video/mp4' />
         </video>
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-starwars.png" alt="disney" />
+        <img src={`${process.env.PUBLIC_URL}/images/viewers-starwars.png`} alt="disney" />
         <video autoPlay loop muted>
-          <source src="/videos/star-wars.mp4" type='video/mp4' />
+          <source src={`${process.env.PUBLIC_URL}/videos/star-wars.mp4`} type='video/mp4' />
         </video>
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-national.png" alt="disney" />
+        <img src={`${process.env.PUBLIC_URL}/images/viewers-national.png`} alt="disney" />
         <video autoPlay loop muted>
-          <source src="/videos/national-geographic.mp4" type='video/mp4' />
+          <source src={`${process.env.PUBLIC_URL}/videos/national-geographic.mp4`} type='video/mp4' />
         </video>
       </Wrap>
     </Container>
@@ -63,7 +63,6 @@ const Wrap = styled.div`
   border: 3px solid rgba(249, 249, 249, 0.1);
   transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
 
-
   img {
     inset: 0px;
     display: block;
@@ -85,17 +84,13 @@ const Wrap = styled.div`
     z-index: 0;
   }
 
-
   &:hover { 
-    box-shadow: rgb(0 0 0 /80%) 0px 40px 58px -16px
-                rgb(0 0 0 /72%) 0px 30px 22px -10px
+    box-shadow: rgb(0 0 0 /80%) 0px 40px 58px -16px,
+                rgb(0 0 0 /72%) 0px 30px 22px -10px;
     transform: scale(1.05);
     border-color: rgba(249, 249, 249, 0.8);
     video {
       opacity: 1;
     }
-  
   }
 `;
-
-
